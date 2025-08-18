df6529af48332611ed0538a4cf108fcf873d1d71
Amy Milewski <amy12milewski@gmail.com>
2025-08-18 14:34:58 -0400

    RCP25.2-3 + ML25.7

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,12 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "MO-PCCO.R3 - NextJS Financial Dashboard",
-  description: "Starter code for Module 25 Capstone project - Financial Dashbard",
-};
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body> 
     </html>
   );
 }
+// Appending the inter.className to the className attribute of the <body> element ensures that the Inter font is applied globally throughout the application.
+// Additionally, the antialiased class from Tailwind CSS is added to provide smoother font rendering, enhancing the visual appearance of the text.
--- src/app/ui/invoices/status.jsx
+++ src/app/ui/invoices/status.jsx
@@ -1,6 +1,7 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
+// The component accepts a prop named status, which is a string representing the status of the invoice (pending or paid).
 export default function InvoiceStatus({ status }) {
   return (
     <span
@@ -9,6 +10,7 @@ export default function InvoiceStatus({ status }) {
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
+          'bg-red-500 text-white': status === 'late'
         },
       )}
     >
@@ -22,6 +24,12 @@ export default function InvoiceStatus({ status }) {
         <>
           Paid
           <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'late' ? (
+        <>
+          Late
+          <ClockIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
     </span>
